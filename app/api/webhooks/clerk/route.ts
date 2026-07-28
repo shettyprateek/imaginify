6a67c3100332a1afcf65473c63f412702da77718
shettyprateek <prateekshetty66@gmail.com>
2026-07-28 07:16:50 +0530

    Added logs

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -8,6 +8,7 @@ import { Webhook } from "svix";
 import { createUser, deleteUser, updateUser } from "@/lib/actions/user.actions";
 
 export async function POST(req: NextRequest) {
+  console.log("✅ Webhook endpoint hit");
   // You can find this in the Clerk Dashboard -> Webhooks -> choose the webhook
   const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
 
