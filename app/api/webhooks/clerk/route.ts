fba1af4610db4e3b962aee32281cb8daad6cda80
shettyprateek <prateekshetty66@gmail.com>
2026-08-04 06:53:25 +0530

    Fixed env variables

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -9,7 +9,7 @@ import { createUser, deleteUser, updateUser } from "@/lib/actions/user.actions";
 export async function POST(req: NextRequest) {
   console.log("✅ Webhook endpoint hit");
   // You can find this in the Clerk Dashboard -> Webhooks -> choose the webhook
-  const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
+  const WEBHOOK_SECRET = process.env.CLERK_WEBHOOK_SIGNING_SECRET;
 
   if (!WEBHOOK_SECRET) {
     throw new Error(
